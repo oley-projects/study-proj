e1267ba3dca37018171b89a071202ebdb2693ad9
Oley-Projects <oley.for.git@gmail.com>
2017-08-01 18:43:45 +0300

    Saving to public

--- 20/barbershop.rb
+++ 20/barbershop.rb
@@ -12,25 +12,9 @@ post '/' do
 	@title = 'Thank you!'
 	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
 
-	f = File.open 'users.txt', 'a'
+	f = File.open './public/users.txt', 'a'
 	f.write "User: #{@user_name}, Phone: #{@phone}, Date: #{@date_time}\n"
 	f.close
 
 	erb :message
-end
-
-get '/admin' do 
-	erb :admin
-end
-
-post '/admin' do
-
-	@login = params[:login]
-	@password = params[:pass]
-	if @login == 'admin' && @password == 'secret'
-		@logfile = File.read("users.txt")
-	else
-		erb :admin
-	end
-
 end
